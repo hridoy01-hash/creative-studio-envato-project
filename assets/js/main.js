e0dc5f7d03e1da7e64584c191f2d4e2dd86ec71c
hridoy01-hash <hridoy.conceptx@gmail.com>
2024-07-14 11:35:04 +0600

    update brand animation section and team section start

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,8 +17,21 @@
         },
     });
 
-    // ed brand scrool animation
-    var swiper = new Swiper(".ed_brand_scroll_active", {
+    // ed brand scrool animation top
+    var swiper = new Swiper(".ed_brand_scroll_active_top", {
+        slidesPerView: 'auto',
+        spaceBetween: 80,
+        freeMode: true,
+        loop: true,
+        speed: 2000, // Adjust this for desired scrolling speed
+        allowTouchMove: false,
+        autoplay: {
+            delay: 0,
+            disableOnInteraction: false,
+        },
+    });
+    // ed brand scrool animation bottom
+    var swiper = new Swiper(".ed_brand_scroll_active_bottom", {
         slidesPerView: 'auto',
         spaceBetween: 80,
         freeMode: true,
